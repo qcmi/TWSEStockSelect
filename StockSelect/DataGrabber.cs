62ecdd19f1803ea5db3c10f5b113c12791b9d859
Alex <alexchang@sinopac.com>
2016-09-08 14:36:05 +0800

    Add OTCDataGrabber

---
+++ StockSelect/DataGrabber.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StockSelect
+{
+    public class DataGrabber
+    {
+        protected List<StockInfo> data_list = new List<StockInfo>();
+        System.Globalization.TaiwanCalendar TC;
+        public bool IsDownloadFinish { get; set; }
+        protected string Url;
+
+        public DataGrabber()
+        {
+            TC = new System.Globalization.TaiwanCalendar();
+            this.IsDownloadFinish = false;
+        }
+
+        public void ClearData()
+        {
+            data_list.Clear();
+        }
+
+        public List<StockInfo> GetStockInfoList()
+        {
+            return this.data_list;
+        }
+
+        public void SetStockInfoList(List<StockInfo> list)
+        {
+            this.data_list.Clear();
+            this.data_list = list;
+        }
+    }
+}
---
+++ StockSelect/OTCDataGrabber.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HtmlAgilityPack;
+
+namespace StockSelect
+{
+    public class OTCDataGrabber : DataGrabber
+    {
+        string UrlEnd;
+        public OTCDataGrabber() : base()
+        {
+            this.Url = "http://www.tpex.org.tw/web/stock/aftertrading/daily_close_quotes/stk_quote_print.php?l=zh-tw&d=";
+            this.UrlEnd = "&s=0,asc,0";
+        }
+
+        public void DownloadData(DateTime queryDate)
+        {
+            System.Globalization.TaiwanCalendar TC = new System.Globalization.TaiwanCalendar();
+
+            string postData = String.Format("{0}/{1}", TC.GetYear(queryDate), queryDate.ToString("MM/dd"));
+
+            string downloadUrl = Url + postData + UrlEnd;
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load(downloadUrl);
+
+            HtmlNodeCollection nameNodes = doc.DocumentNode.SelectNodes("//table//tbody");
+            foreach (HtmlNode table in nameNodes)
+            {
+                Console.WriteLine("Found: " + table.Id);
+                foreach (HtmlNode row in table.SelectNodes("tr"))
+                {
+                    if (row.SelectNodes("th|td")[0].InnerText != "管理股票")
+                    {
+                        if (row.SelectNodes("th|td")[0].InnerText.Length == 4 || row.SelectNodes("th|td")[0].InnerText == "006201")
+                        {
+                            for (int i = 0; i < 6; i++)
+                            {
+                                Console.WriteLine(row.SelectNodes("th|td")[i].InnerText);
+                            }
+                        }                      
+                    }
+                }
+            }
+        }
+    }
+}
